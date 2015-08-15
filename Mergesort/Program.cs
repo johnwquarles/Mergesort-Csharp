f62570898ca259a5ef33acc187c07f1d2e7f806d
johnwquarles <john.w.quarles@gmail.com>
2015-08-15 13:58:21 -0500

    initial commit

---
+++ Mergesort/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("compiled");
+        }
+
+        static public int[] Mergesort(int[] arr)
+        {
+            if (arr.Length == 1) return arr;
+            int midPt = (int)decimal.Floor(arr.Length / 2);
+
+            int[] left = new int[midPt];
+            int[] right = new int[arr.Length - midPt];
+
+            // source, start index in source, dest, start index in dest, how many to copy
+            Array.Copy(arr, 0, left, 0, midPt);
+            Array.Copy(arr, midPt, right, 0, arr.Length - midPt);
+
+            int[] leftMergesorted = Mergesort(left);
+            int[] rightMergesorted = Mergesort(right);
+
+            return Merge(leftMergesorted, rightMergesorted);
+        }
+
+        static public int[] Merge(int[] left, int[] right)
+        {
+            // base cases
+            if (left.Length == 0) return right;
+            if (right.Length == 0) return left;
+
+            int[] returnArr = new int[left.Length + right.Length];
+
+            if (left[0] <= right[0])
+            {
+                int[] singleton = new int[] { left[0] };
+
+                int[] newLeft = new int[left.Length - 1];
+                // source, start index in source, dest, start index in dest, how many to copy
+                Array.Copy(left, 1, newLeft, 0, left.Length - 1);
+
+                int[] theRest = Merge(newLeft, right);
+                singleton.CopyTo(returnArr, 0);
+                theRest.CopyTo(returnArr, 1);
+            } else {
+                int[] singleton = new int[] { right[0] };
+
+                int[] newRight = new int[right.Length - 1];
+                // source, start index in source, dest, start index in dest, how many to copy
+                Array.Copy(right, 1, newRight, 0, right.Length - 1);
+
+                int[] theRest = Merge(left, newRight);
+                singleton.CopyTo(returnArr, 0);
+                theRest.CopyTo(returnArr, 1);
+
+            }
+            return returnArr;
+        }
+    }
+}
---
+++ MergesortTests/UnitTest1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MergeSort;
+
+namespace MergeSortTests
+{
+    [TestClass]
+    public class Merge
+    {
+        [TestMethod]
+        public void TestBaseCaseLeft()
+        {
+            int[] a = new int[] { 1 };
+            int[] b = new int[0];
+            CollectionAssert.AreEqual(a, Program.Merge(a, b));
+        }
+
+        [TestMethod]
+        public void TestBaseCaseRight()
+        {
+            int[] a = new int[0];
+            int[] b = new int[] { 1 };
+            CollectionAssert.AreEqual(b, Program.Merge(a, b));
+        }
+
+        [TestMethod]
+        public void TestSingletons()
+        {
+            int[] a = new int[] { 2 };
+            int[] b = new int[] { 1 };
+            int[] expect = new int[] { 1, 2 };
+            CollectionAssert.AreEqual(expect, Program.Merge(a, b));
+        }
+
+        [TestMethod]
+        public void TestKindaComplicated()
+        {
+            int[] a = new int[] { 2, 7, 19, 22 };
+            int[] b = new int[] { 1, 18, 20, 21, 44 };
+            int[] expect = new int[] { 1, 2, 7, 18, 19, 20, 21, 22, 44 };
+            CollectionAssert.AreEqual(expect, Program.Merge(a, b));
+        }
+
+        [TestMethod]
+        public void TestAllOnOneSide()
+        {
+            int[] a = new int[] { 0, 1, 2, 3, 4, 5 };
+            int[] b = new int[] { 6, 7, 8 };
+            int[] expect = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            CollectionAssert.AreEqual(expect, Program.Merge(a, b));
+        }
+    }
+
+    [TestClass]
+    public class Mergesort
+    {
+        [TestMethod]
+        public void TestSimpleOdd()
+        {
+            int[] a = new int[] { 1, 6, 2, 0, 3 };
+            int[] expect = new int[] { 0, 1, 2, 3, 6 };
+            CollectionAssert.AreEqual(expect, Program.Mergesort(a));
+        }
+
+        [TestMethod]
+        public void TestSimpleEven()
+        {
+            int[] a = new int[] { 1, 6, 2, 0, 3, 4 };
+            int[] expect = new int[] { 0, 1, 2, 3, 4, 6 };
+            CollectionAssert.AreEqual(expect, Program.Mergesort(a));
+        }
+
+        [TestMethod]
+        public void alreadyInOrder()
+        {
+            int[] a = new int[] { 0,2,5,7,9 };
+            int[] expect = new int[] { 0, 2,5,7,9 };
+            CollectionAssert.AreEqual(expect, Program.Mergesort(a));
+        }
+
+        [TestMethod]
+        public void backwards()
+        {
+            int[] a = new int[] { 400,300,20,5,2,1 };
+            int[] expect = new int[] { 1,2,5,20,300,400 };
+            CollectionAssert.AreEqual(expect, Program.Mergesort(a));
+        }
+    }
+}
